39ffc1c4c36efc318073ad4b91e5120f0d1946e1
SangwanYu <ryusangwan12@gmail.com>
2025-11-27 22:20:41 +0900

    design: 카테고리 버튼 디자인 변경

--- src/app/onboarding/components/categoruButton.tsx
+++ src/app/onboarding/components/categoruButton.tsx
@@ -3,6 +3,8 @@
 import { CategorySquareBadge } from "@/components/ui/category/CategorySquareBadge";
 import {
 	categoryLabels,
+	categoryHoverColors,
+	categorySelectedColors,
 	type CategoryType,
 } from "@/components/ui/category/config";
 import { cn } from "@/utils/cn";
@@ -18,18 +20,23 @@ export default function CategoryButton({
 	isSelected,
 	onClick,
 }: CategoryButtonProps) {
+	const hoverStyle = categoryHoverColors[category];
+	const activeStyle = categorySelectedColors[category];
+
 	return (
-		<div
+		<button
 			onClick={onClick}
 			className={cn(
-				"glass flex h-36 w-44 cursor-pointer flex-col items-center justify-center rounded-lg transition-all",
-				isSelected ? "bg-white/20" : "bg-white/5 hover:bg-white/10"
+				"glass flex h-36 w-44 cursor-pointer flex-col items-center justify-center rounded-lg outline-[0.25px] outline-offset-[-0.25px] transition-all",
+				isSelected
+					? `${activeStyle} outline-white`
+					: `outline-white-30 bg-white/5 ${hoverStyle} hover:outline-white-50`
 			)}
 		>
 			<CategorySquareBadge category={category} />
 			<span className="text-subtitle3 mt-4 text-white">
 				{categoryLabels[category]}
 			</span>
-		</div>
+		</button>
 	);
 }
