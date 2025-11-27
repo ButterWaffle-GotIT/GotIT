64d347ad9ca7e6a86923aaf8970b0e7320dd2e50
hyeseong <165576376+hyesngy@users.noreply.github.com>
2025-11-27 19:40:05 +0900

    [Feat/#28] 구글 소셜 로그인 연동 (#32)
    * feat: update next.config.ts
    
    * feat: google social login
    
    * fix: lint error
    
    * chore: update workflows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useCallback } from "react";
-import Header from "@/components/Header";
 import SearchBar from "@/components/SearchBar";
 import TagList from "@/components/TagList";
 import RecommendedTermCard from "@/components/RecommendedTermCard";
@@ -42,7 +41,7 @@ export default function HomePage() {
 
 	return (
 		<>
-			<Header />
+			{/* <Header /> */}
 
 			<main className="flex w-full justify-center pt-20">
 				<div className="flex w-[1040px] flex-col items-center gap-20 px-20 pt-48 pb-20">
