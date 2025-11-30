b7411736e61943daa878e70d5a7675410d317bf4
hyeseong <tell280210@gmail.com>
2025-11-30 13:07:25 +0900

    refactor: remove unused files

--- src/components/landing/MarqueeSection.tsx
+++
@@ -1,9 +0,0 @@
-import { MarqueeText } from "./MarqueeText";
-
-export function MarqueeSection() {
-	return (
-		<section className="flex h-screen snap-start snap-always items-center overflow-hidden border-y border-gray-800 px-12 py-6 md:px-24 lg:px-60">
-			<MarqueeText />
-		</section>
-	);
-}
