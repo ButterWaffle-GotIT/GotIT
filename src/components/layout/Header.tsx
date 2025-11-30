b2faa405fdc037d0fc72b21908682f1b4a985b44
hyeseong <tell280210@gmail.com>
2025-11-30 13:25:43 +0900

    fix: 로그인 버튼 텍스트 크기 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -47,7 +47,7 @@ const NavItem = ({
 
 const LoginButton = ({ onClick }: { onClick: () => void }) => (
 	<GlassButton className="px-6 py-2" onClick={onClick}>
-		<span className="text-lg leading-7 font-bold text-neutral-300 transition-colors hover:text-white">
+		<span className="text-base leading-7 font-bold text-neutral-300 transition-colors hover:text-white">
 			로그인
 		</span>
 	</GlassButton>
